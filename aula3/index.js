5b38f95efbc1be4c628eec1d36e253cfe80ab450
Elthon <elthonferreira18@gmail.com>
2022-08-24 20:46:07 -0300

    Iniciando os trabalhos

---
+++ aula3/index.js
@@ -0,0 +1,10 @@
+console.log('Elthon "Ferreira"');   // Aspas duplas dentro de aspas simples
+console.log("Elthon 'Ferreira'");   // Aspas simples dentro de aspas duplas
+console.log(`'Elthon' "Ferreira"`); // Aspas simples e duplas dentro de crases 
+
+console.log(26, 74.1, 'Elthon Ferreira de novo');
+
+// String = Texto
+// Number = Número
+
+// Não tem separação número em ponto flutuante e número inteiro, tudo é number.
