dfaefa527261f242fff3a7d9c53c1110be5569cd
Elthon <elthonferreira18@gmail.com>
2022-08-24 22:23:35 -0300

    União de html + javascript

---
+++ aula5/js/index.js
@@ -0,0 +1,6 @@
+   // Um comentário
+
+   console.log('Olá meu fera');
+   console.log('Este trecho será exibido no console do navegador!');
+
+   alert('Esse código não está definido no vscode, mas funciona no navegador.');
