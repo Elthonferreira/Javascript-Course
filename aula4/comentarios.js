5be15c1d115825d9c60d6b9df452aefddc1edfa4
Elthon <elthonferreira18@gmail.com>
2022-08-24 21:34:28 -0300

    Comentários

---
+++ aula4/comentarios.js
@@ -0,0 +1,18 @@
+// Escrever um comentário
+console.log('Olá mundo!'); // Aqui tem outro comentário
+
+console.log('Linha 1'); 
+/* Comentário 
+longo */
+
+
+/**
+ * Um título...
+ * 
+ * Lorem ipsum
+ */
+//console.log('Linha 2');
+console.log('Linha 3');
+console.log('Linha 4');
+
+// Vai ser ignorado
