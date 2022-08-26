c35841cb24057bfdd0265e6df76a2ce1b5414076
Elthon <elthonferreira18@gmail.com>
2022-08-26 20:26:54 -0300

    tipos de dados primitivos

---
+++ aula9/difference-between-let-var.js
@@ -0,0 +1,9 @@
+// ECMAScript 2015 (ES6)
+
+var nome = 'Elthon'; 
+var nome = 'Ferreira';
+console.log(nome);
+
+// var pode redeclarar a variável (não indicado fazer isso)
+// let não deixa redeclarar
+
--- aula9/index.js
+++ aula9/index.js
@@ -1,9 +1,27 @@
-// ECMAScript 2015 (ES6)
+// tipos primitivos
+// String, number, undefined, null, boolean, symbol
 
-var nome = 'Elthon'; 
-var nome = 'Ferreira';
-console.log(nome);
+const nome = 'Elthon';     // string
+const nome1 = "Elthon";    // string
+const nome2 = `Elthon`;    // string
 
-// var pode redeclarar a variável (não indicado fazer isso)
-// let não deixa redeclarar
+const num1 = 10;           // number
+const num2 = 10.52;        // number
 
+let nomeAluno;               // underfined -> não aponta para local nenhum na memória
+const sobrenomeAluno = null; // Nulo -> não aponta para local nenhum na memória
+const aprovado = true;       // Boolean = true, false (lógico)
+
+console.log(typeof num1);    // verificar tipo da variável
+ 
+let numeroA = 2;
+let numeroB = numeroA;          // gera uma cópia de 'numeroA' para 'numeroB'
+console.log(numeroA, numeroB);  // 2, 2
+numeroA = 3;
+console.log(numeroA, numeroB);  // 3, 2
+
+const arrayA = [1, 2];
+const arrayB = arrayA;          // valor por referência (arrayB aponta para o mesmo lugar que arrayA)
+console.log(arrayA, arrayB);    // [ 1, 2 ] [ 1, 2 ]
+arrayB.push(3);
+console.log(arrayA, arrayB);    // [ 1, 2, 3 ] [ 1, 2, 3 ]
