fc7d16b0d44a189523867b1a2f3ef6f85c2bb4c5
Elthon <elthonferreira18@gmail.com>
2022-08-29 00:43:00 -0300

    exercício prompt/alert

--- aula11/js/script.js
+++ aula11/js/script.js
@@ -1,11 +1,13 @@
-alert('teste');
+//alert('teste');
 
-const num1 = prompt('Digite um número');
-const num2 = prompt('Digite outro número');
+let num1 = prompt('Digite um número');
+let num2 = prompt('Digite outro número');
 
 num1 = Number(num1);
-num2 = Number(num2);
-const resultado = num1 + num2;
+num2 = Number(num2)
+//const resultado = num1 + num2;
 
 
-alert('O resultado foi:' + resultado);
+//alert('O resultado foi: ' + resultado);
+//alert(`O resultado é: ${resultado}`);
+alert(`O resultado é: ${num1 + num2}`);
