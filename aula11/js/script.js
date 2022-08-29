2784bd892922e35f25f665949b52649936bc0a56
Elthon <elthonferreira18@gmail.com>
2022-08-28 23:55:17 -0300

    alert, confrm e prompt

---
+++ aula11/js/script.js
@@ -0,0 +1,11 @@
+alert('teste');
+
+const num1 = prompt('Digite um número');
+const num2 = prompt('Digite outro número');
+
+num1 = Number(num1);
+num2 = Number(num2);
+const resultado = num1 + num2;
+
+
+alert('O resultado foi:' + resultado);
