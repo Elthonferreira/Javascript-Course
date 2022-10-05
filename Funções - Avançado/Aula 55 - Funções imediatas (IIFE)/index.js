c5074271f03986c652ea282b3a790914272e3d59
Elthon <elthonferreira18@gmail.com>
2022-10-05 20:47:50 -0300

    Funções imediatas (IIFE)

---
+++ Funções - Avançado/Aula 55 - Funções imediatas (IIFE)/index.js
@@ -0,0 +1,9 @@
+// IIFE -> Immediately invoked function expression
+
+(function() { // Função invocada imediatamente (não toca o escopo global)
+    const nome = 'Elthon1';
+    console.log(nome);
+    console.log(123);
+})();
+
+const nome = 'Elthon2';
