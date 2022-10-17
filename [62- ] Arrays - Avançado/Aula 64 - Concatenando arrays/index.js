e1a1ea7b3a83662b8f26d8a8e00452c7d31b8a79
Elthon <elthonferreira18@gmail.com>
2022-10-16 23:52:52 -0300

    Concatenando arrays

---
+++ [62- ] Arrays - Avançado/Aula 64 - Concatenando arrays/index.js
@@ -0,0 +1,11 @@
+const a1 = [1, 2, 3];
+const a2 = [4, 5, 6];
+
+const a3 = a1.concat(a2, [7, 8, 9], 'Elthon');
+
+// ... rest -> ... spread
+
+const a4 = [... a1, 'Elthon', ...a2, ...[7, 8, 9 ]];
+
+console.log(a3);
+console.log(a4);
