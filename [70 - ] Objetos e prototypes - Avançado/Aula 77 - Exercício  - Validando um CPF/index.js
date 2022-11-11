c066d0a0a15e774860fc144da7eac2524cedcf41
Elthon <elthonferreira18@gmail.com>
2022-11-10 22:25:44 -0300

    Validação de CPF - Elthon

--- [70 - ] Objetos e prototypes - Avançado/Aula 77 - Exercício  - Validando um CPF/index.js
+++ [70 - ] Objetos e prototypes - Avançado/Aula 77 - Exercício  - Validando um CPF/index.js
@@ -19,11 +19,80 @@ let cpf = '705.484.450-52';
 let cpfLimpo = cpf.replace(/\D+/g, ''); // expressão regular que tem a representação numérica de qualquer coisa que não é um número
 let cpfArray = Array.from(cpfLimpo);
 
-console.log(cpf);
-console.log(cpfLimpo);
-console.log(Array.from(cpfLimpo));
+function somatorioParte1(cpf) {
+    let cpfLimpo = cpf.replace(/\D+/g, '');
+    let somatorio = 0;
+    let auxPrimeiroDigito = 10;
+    for (let i of cpfLimpo) {
+        somatorio = somatorio + (i * auxPrimeiroDigito);
+        auxPrimeiroDigito--;
+        if (auxPrimeiroDigito < 2) break;
+    }
+    return somatorio;
+}
 
-console.log(cpfArray.reduce((ac, val) => ac + Number(val), 0))
+function verificaParte1(cpf, somatorioCPF) {
+    let cpfLimpo = cpf.replace(/\D+/g, '');
+    let primeiroDigito;
 
+    if (11 - (somatorioCPF % 11) > 9) primeiroDigito = 0;
+    else primeiroDigito = (11 - (somatorioCPF % 11));
 
+    console.log('primeiro digito calculo: ' + primeiroDigito);
+    console.log('primeiro digito cpfLimpo: ' + cpfLimpo[9]);
+
+    if (primeiroDigito == cpfLimpo[9]) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function somatorioParte2(cpf) {
+    let cpfLimpo = cpf.replace(/\D+/g, '');
+    let somatorio = 0;
+    let auxSegundoDigito = 11;
+    for (let i of cpfLimpo) {
+        somatorio = somatorio + (i * auxSegundoDigito);
+        auxSegundoDigito--;
+        if (auxSegundoDigito < 2) break;
+    }
+    return somatorio;
+}
+
+function verificaParte2(cpf, somatorioCPF) {
+    let cpfLimpo = cpf.replace(/\D+/g, '');
+    let segundoDigito;
+
+    if (11 - (somatorioCPF % 11) > 9) segundoDigito = 0;
+    else segundoDigito = (11 - (somatorioCPF % 11));
+
+    console.log('Segundo digito calculo: ' + segundoDigito);
+    console.log('Segundo digito cpfLimpo: ' + cpfLimpo[10]);
+
+    if (segundoDigito == cpfLimpo[10]) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function validarCPF(cpf) {
+    // somatorioParte1(cpf);
+    console.log(somatorioParte1(cpf));
+    console.log(verificaParte1(cpf, somatorioParte1(cpf)));
+    console.log(somatorioParte2(cpf));
+    console.log(verificaParte2(cpf, somatorioParte2(cpf)));
+    if (verificaParte1(cpf, somatorioParte1(cpf)) && verificaParte2(cpf, somatorioParte2(cpf))) {
+        return 'CPF válido.';
+    } else {
+        return 'CPF inválido.';
+    }
+}
+
+console.log(validarCPF('719.716.720-50'));
+
+// código acima feito por mim Elthon
+
+// código abaixo feito pelo professor
 
